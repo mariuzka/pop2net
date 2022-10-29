3687816a8b3cde3d0b39dc28c8356ad5e7ea6492
Lukas Erhard <lukas.erhard@sowi.uni-stuttgart.de>
2022-10-29 14:22:21 +0200

    assert that SoepProvider actually returns a correctly sized df

--- tests/conftest.py
+++ tests/conftest.py
@@ -121,36 +121,42 @@ def soep_dataset_generator(size: int) -> pd.DataFrame:
 
     # create correlated variables, dependent on child vs. adult
     cases = []
-    for _ in range(1, size + 1):
-        hh_id, hh_size, n_children = fake.household()
-        n_adults = hh_size - n_children
-        for _ in range(n_adults):
-            case = {
-                "pid": fake.unique.random_int(min=1, max=size * 10),
-                "hid": hh_id,
-                "age": fake.age(person="adult"),
-                "gender": fake.gender(),
-                "work_hours_day": fake.work_hours_per_day(person="adult"),
-            }
-            cases.append(case)
-            if len(cases) == size:
-                break
-        for _ in range(n_children):
-            case = {
-                "pid": fake.unique.random_int(min=1, max=size * 10),
-                "hid": hh_id,
-                "age": fake.age(person="child"),
-                "gender": fake.gender(),
-                "work_hours_day": fake.work_hours_per_day(person="child"),
-            }
-            cases.append(case)
-            if len(cases) == size:
-                break
-        if len(cases) == size:
-            break
+    try:
+        for _ in range(1, size + 1):
+            hh_id, hh_size, n_children = fake.household()
+            n_adults = hh_size - n_children
+            for _ in range(n_adults):
+                case = {
+                    "pid": fake.unique.random_int(min=1, max=size * 10),
+                    "hid": hh_id,
+                    "age": fake.age(person="adult"),
+                    "gender": fake.gender(),
+                    "work_hours_day": fake.work_hours_per_day(person="adult"),
+                }
+                cases.append(case)
+                if len(cases) == size:
+                    raise StopIteration
+            for _ in range(n_children):
+                case = {
+                    "pid": fake.unique.random_int(min=1, max=size * 10),
+                    "hid": hh_id,
+                    "age": fake.age(person="child"),
+                    "gender": fake.gender(),
+                    "work_hours_day": fake.work_hours_per_day(person="child"),
+                }
+                cases.append(case)
+                if len(cases) == size:
+                    raise StopIteration
+    except StopIteration:
+        pass
 
     df = pd.DataFrame(cases)
     return df
+
+
+@pytest.fixture(scope="session")
+def soep10():
+    return soep_dataset_generator(10)
 
 
 @pytest.fixture(scope="session")
--- tests/test_data_faker.py
+++ tests/test_data_faker.py
@@ -1,6 +1,20 @@
 import pytest
 
 
+@pytest.mark.parametrize(
+    "soep_fixture,size",
+    [
+        ("soep100", 100),
+        ("soep1000", 1000),
+        ("soep10_000", 10_000),
+    ],
+)
+def test_dataframe_size(soep_fixture, size, request):
+    print(soep_fixture)
+    soep = request.getfixturevalue(soep_fixture)
+    assert len(soep) == size
+
+
 @pytest.mark.parametrize("soep_fixture", ["soep100", "soep1000", "soep10_000"])
 def test_pid_unique(soep_fixture, request):
     soep = request.getfixturevalue(soep_fixture)
