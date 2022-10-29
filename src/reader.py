7af9f87f392b832aeb038f4e95f289d6285c183c
Lukas Erhard <lukas.erhard@sowi.uni-stuttgart.de>
2022-10-29 17:28:38 +0200

    implement sampling without replacement
    - use numpy for sampling, more flexible and possibly faster than random
    
    implement sampling without replacement edgecases

--- src/reader.py
+++ src/reader.py
@@ -1,6 +1,6 @@
-import random
 from typing import Optional
 
+import numpy as np
 import pandas as pd
 
 
@@ -12,36 +12,60 @@ class DataReader:
     ) -> None:
 
         if seed:
-            self.rng = random.Random(seed)
+            self.rng = np.random.default_rng(seed)
         else:
-            self.rng = random.Random()
+            self.rng = np.random.default_rng(None)
 
         self.df = df
 
-    def sample(self, by: str, size: int, weights: Optional[str] = None) -> pd.DataFrame:
+    def sample(
+        self,
+        by: str,
+        size: int,
+        weights: Optional[str] = None,
+        with_replacement: bool = True,
+    ) -> pd.DataFrame:
 
         if weights is None:
             unique_by = self.df[[by]].drop_duplicates(subset=by, keep="first")
             weight_col = None
         else:
             unique_by = self.df[[by, weights]].drop_duplicates(subset=by, keep="first")
-            weight_col = unique_by[weights].tolist()
+            weight_col = unique_by[weights]
 
-        id_col = unique_by[by].tolist()
-        sample = self.rng.choices(id_col, weights=weight_col, k=size)
+        id_col = unique_by[by]
+        if with_replacement or (size <= len(id_col)):
+            sample = self.rng.choice(id_col, p=weight_col, replace=with_replacement, size=size)
+        elif size <= len(self.df):
+            # edge case:
+            # sampling without replacement and non-unique sampling column while size
+            # is larger than number of unique values to sample from
+            sample = id_col.tolist()  # type: ignore
+            self.rng.shuffle(sample)
+        else:
+            raise ValueError(
+                f"Cannot sample size ({size}) larger than dataset ({len(self.df)}) "
+                "without replacement!",
+            )
 
-        subset_dfs = []
-        len_counter = 0
-        for sample_id in sample:
+        if not with_replacement:
+            # Faster shortcut that is possible if sampling is done without replacement.
+            # Therefore implemented separately.
+            df = self.df.loc[self.df[by].isin(sample), :]
+            df = df.iloc[:size]
+        else:
+            subset_dfs = []
+            len_counter = 0
+            for sample_id in sample:
 
-            subset = self.df[self.df[by] == sample_id]
-            subset_dfs.append(subset)
+                subset = self.df[self.df[by] == sample_id]
+                subset_dfs.append(subset)
 
-            len_counter += len(subset)
-            if len_counter >= size:
-                break
+                len_counter += len(subset)
+                if len_counter >= size:
+                    break
 
-        df = pd.concat(subset_dfs, axis=0)
-        df = df.iloc[:size]
+            df = pd.concat(subset_dfs, axis=0)
+            df = df.iloc[:size]
 
         return df
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -15,3 +15,36 @@ def test_sample_size(size, soep_fixture, request):
     reader = DataReader(soep, seed=5)
     sample = reader.sample(by="hid", size=size, weights=None)
     assert len(sample) == size
+
+
+def test_sample_by_str_col(soep100):
+    soep100.pid = soep100.pid.astype(str)
+    reader = DataReader(soep100, seed=5)
+    sample = reader.sample(by="pid", size=10, with_replacement=False)
+    assert sample.pid.nunique() == 10
+
+
+@pytest.mark.parametrize("size", [10, 50, 99, 100])
+def test_sample_no_replacement(soep100, size):
+    reader = DataReader(soep100, seed=5)
+    sample = reader.sample(by="pid", size=size, with_replacement=False)
+    assert sample.pid.nunique() == size
+
+
+def test_sample_no_replacement_grouped_edgecase(soep100):
+    reader = DataReader(soep100, seed=5)
+    sample = reader.sample(by="hid", size=99, with_replacement=False)
+    assert len(sample) == 99
+    assert sample.hid.nunique() < 99
+
+
+def test_sample_no_replacement_too_many(soep100):
+    reader = DataReader(soep100, seed=5)
+    with pytest.raises(ValueError):
+        reader.sample(by="pid", size=101, with_replacement=False)
+
+
+def test_sample_with_replacement_too_many(soep100):
+    reader = DataReader(soep100, seed=5)
+    sample = reader.sample(by="pid", size=101, with_replacement=True)
+    assert len(sample) == 101
