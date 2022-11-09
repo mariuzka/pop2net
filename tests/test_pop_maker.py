0e668e8075d5aa8a2170057f09a2f2d1c76e6971
Marius Kaffai <marius.kaffai@gmail.com>
2022-11-09 10:31:26 +0100

    Add 'parametrize' to first PopMaker test

--- tests/test_pop_maker.py
+++ tests/test_pop_maker.py
@@ -8,17 +8,19 @@ class Model(ap.Model):
     pass
 
 
-class TestAgent(ap.Agent):
+class Agent(ap.Agent):
     pass
 
 
-def test_create_agents(soep100):
+@pytest.mark.parametrize("soep_fixture", ["soep100", "soep1000", "soep10_000"])
+def test_create_agents(soep_fixture, request):
+    soep = request.getfixturevalue(soep_fixture)
 
-    pop_maker = PopMaker(df=soep100, agent_class=TestAgent, agent_params={"model": Model()})
+    pop_maker = PopMaker(df=soep, agent_class=Agent, agent_params={"model": Model()})
     agents = pop_maker.create_agents()
 
-    assert len(agents) == len(soep100)
+    assert len(agents) == len(soep)
 
-    for i, row in soep100.iterrows():
-        for col_name in soep100.columns:
+    for i, row in soep.iterrows():
+        for col_name in soep.columns:
             assert row[col_name] == getattr(agents[i], col_name)
