37dde88c289f5061eafc048959e56b059b9d30fc
Lukas Erhard <luerhard@googlemail.com>
2022-11-30 12:58:33 +0100

    add autodoc extensions with google-style support

--- docs/conf.py
+++ docs/conf.py
@@ -8,15 +8,34 @@
 project = "popy"
 copyright = "2022, Marius Kaffai, Lukas Erhard"
 author = "Marius Kaffai, Lukas Erhard"
-release = "0.0.1"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# Napoleon settings
+# For a list of possible configurations, see:
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html#module-sphinx.ext.napoleon
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
 
 
 # -- Options for HTML output -------------------------------------------------
