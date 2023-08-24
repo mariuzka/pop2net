99f3f0ee6656e53be0e4c97bba122044b96aadf4
Lukas Erhard <lukas.erhard@sowi.uni-stuttgart.de>
2023-08-24 10:43:10 +0200

    fix ruff errors in agent.py
    close #27

--- src/popy/agent.py
+++ src/popy/agent.py
@@ -1,3 +1,5 @@
+"""Base class to create Agent objects."""
+
 import agentpy as ap
 
 from .location import Location
@@ -12,6 +14,10 @@ class Agent(ap.Agent):
     """
 
     def __init__(self, model, *args, **kwargs) -> None:
+        """Agent Constructor.
+
+        All parameters will be passed to the :class:`agentpy.Agent` parent.
+        """
         super().__init__(model, *args, **kwargs)
 
         self.model = model
@@ -19,12 +25,15 @@ class Agent(ap.Agent):
         self.setup()
 
     def setup(self) -> None:
-        """Setup function for the Agent.
+        """Instantiate an Agent.
+
         This is executed on the instantiation of each agent.
         """
 
-    def neighbors(self, duplicates=False) -> ap.AgentList:
-        """Convenience method that returns all neighbors over all locations this agent is currently
+    def neighbors(self) -> ap.AgentList:
+        """Return all neighbors of an agent.
+
+        Convenience method that returns all neighbors over all locations this agent is currently
         located in.
 
         Returns:
@@ -34,15 +43,27 @@ class Agent(ap.Agent):
         return self.model.env.neighbors_of_agent(self)
 
     def add_location(self, location: Location) -> None:
+        """Add this Agent to a given location."""
         self.model.env.add_agent_to_location(self, location)
 
     @property
     def locations(self) -> ap.AgentList:
+        """Return a list of locations that this agent is associated with.
+
+        Returns:
+        -------
+            :class:`popy.LocationList`: A list of locations.
+        """
         return self.model.env.locations_of_agent(self)
 
     def contact_weight(self, agent_v: "Agent") -> float:
-        """Returns the contact weight between this agent and a given other agent, summed over all
-        locations shared.
+        """Return the contact weight between this agent and a given other agent.
+
+        This is summed over all shared locations.
+
+        Returns:
+        -------
+            :float: A weight of the contact between the two agents.
         """
         contact_weight = 0
         for location in self.locations:
