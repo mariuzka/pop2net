6a7b2763b9967fa2682719524a79c7b16a5a7fee
m <marius.kaffai@sowi.uni-stuttgart.de>
2024-03-13 12:44:41 +0100

    Max' updates for location_information() and location_crosstab()

--- src/popy/utils.py
+++ src/popy/utils.py
@@ -224,35 +224,25 @@ def plot_network(
 
 
 def location_information(
-        locations,
-        select_locations: type[popy.Location] = None,
-        agent_attributes:str = None,
-        ):
-    
-    """
-    Parameter
+        locations: list[popy.Location],
+        select_locations: popy.Location | list[popy.Location] | None = None,
+        agent_attributes: str | None | list[str] | None = None,
+        output_format:str = "table",
+) -> None | pd.DataFrame:
+    """Parameter
     - locations =>  Liste aller location Instanzen des Netzwerkes (streng genommen reicht auch Liste von allen die man sich anschauen will)
     - select_locations => einzelner Konstruktor oder Liste von Klassen Konstruktoren
-    - agent_attributes => Liste oder einzelnenes Attribute der Agenten, die in der Output-Tabelle erscheinen sollen
+    - agent_attributes => Liste oder einzelnenes Attribute der Agenten, die in der Output-Tabelle erscheinen sollen.
 
-    Output: Pro Location Instance der ausgewählten Location Klassen werden 
-            Pandas Dataframes (Columns gefiltert durch agent_attributes) 
+    Output: Pro Location Instance der ausgewählten Location Klassen werden
+            Pandas Dataframes (Columns gefiltert durch agent_attributes)
             mit Hilfe von tabulate als Konsolen print ausgegeben
 
     """
-    # TODO Case einfügen, dass man nur locations angibt und dann alle
-    # stats für alle locations sieht? 
-
-    #locations = tuple(locations)
-
-    # Unifiy parameter types
     if select_locations:
         if not isinstance(select_locations, list):
-            # TESTING HOW TO MAKE LIST FROM CONSTRUCTOR
             select_locations = [select_locations]
-    #else:
-        #print("Error: Select at least one location for stats overview")
-        #return
+
     if agent_attributes:
         if not isinstance(agent_attributes, list):
             agent_attributes = [agent_attributes]
@@ -261,13 +251,13 @@ def location_information(
     valid_locations = []
     if select_locations:
         for location_instance in locations:
-            #str_location_instance = str(location_instance).split(" ")[0]
+
             for locationtype in select_locations:
                 if isinstance(location_instance, locationtype):
                     valid_locations.append(location_instance)
     else:
-        valid_locations = [location_instance for location_instance in locations]
-        
+        valid_locations = list(locations)
+
 
     # create agent df per location instance
     agent_dfs = {}
@@ -277,32 +267,50 @@ def location_information(
 
             # Create the title of printout
             title = f'{i+1}.Location: {str(location_instance).split(" ")[0]}'
-            
+            location_type = str(location_instance).split(" ")[0]
             # get all agents per location instance, subset df by agent-attributes
             df = get_df_agents(agents = location_instance.agents)
-            df = df[[c for c  in agent_attributes]]
+            df = df[list(agent_attributes)]
+            df["location_type"] = location_type
             agent_dfs[title] = df
     else:
 
         for i, location_instance in enumerate(valid_locations):
             title = f'{i+1}.Location: {str(location_instance).split(" ")[0]}'
+            location_type = str(location_instance).split(" ")[0]
             df = get_df_agents(agents = location_instance.agents)
             df.drop(df.iloc[:,0:7], axis = 1, inplace = True)
+            df["location_type"] = location_type
             agent_dfs[title] = df
 
+    if output_format == "table":
     #### Print Part "Basic"
-    for  title, df in agent_dfs.items():
-        print(f'{title} \n')
-        print(tabulate(df, headers="keys", tablefmt="fancy_grid")) 
-        print("\n")  
+        for  title, df in agent_dfs.items():
+            print(f"{title} \n")
+            print(tabulate(df, headers="keys", tablefmt="fancy_grid"))
+            print("\n")
+
+    if output_format == "df":
+        location_id_counter = 0
+        df_list = []
+        for title, df in agent_dfs.items():
+            df.insert(0, "location_id", [location_id_counter]*len(df.index))
+            location_id_counter += 1
+            df_list.append(df)
+        return pd.concat(df_list, ignore_index=True)
+    return None
+
+
+
 
 
 def location_crosstab(
-        locations,
-        select_locations,
-        agent_attributes:str,
-        ):
-    # Make every Parameter a list 
+        locations: list[popy.Location],
+        select_locations: popy.Location | list[popy.Location],
+        agent_attributes: str | list[str],
+        output_format = "table",
+)-> list[pd.DataFrame] | None:
+    # Make every Parameter a list
     if select_locations:
         if not isinstance(select_locations, list):
             select_locations = [select_locations]
@@ -315,39 +323,90 @@ def location_crosstab(
     valid_locations = []
     if select_locations:
         for location_instance in locations:
-            
+
             for locationtype in select_locations:
                 if isinstance(location_instance, locationtype):
                     valid_locations.append(location_instance)
     else:
-        valid_locations = [location_instance for location_instance in locations]
-        
+        valid_locations = list(locations)
+
 
     # create agent df per location instance
-    agent_dfs = {}
-    if agent_attributes:
+    if output_format == "table":
+        agent_dfs = {}
+        if agent_attributes:
 
+            for i, location_instance in enumerate(valid_locations):
+
+                title = f'{i+1}.Location: {str(location_instance).split(" ")[0]}'
+                df = get_df_agents(agents=location_instance.agents)
+                df = df[list(agent_attributes)]
+                agent_dfs[title] = df
+
+        for title, df in agent_dfs.items():
+
+            for agent_attribute in agent_attributes:
+
+                crosstab_table = pd.crosstab(
+                    index= df[agent_attribute],
+                    columns="count",
+                )
+
+                print(f"{title}")
+                print(
+                    tabulate(
+                        crosstab_table,
+                        headers=[agent_attribute, "count"],
+                        tablefmt="fancy_grid",
+                    ),
+                )
+                print("\n")
+    if output_format == "df":
+        agent_dfs = {}
+        result_list = []
         for i, location_instance in enumerate(valid_locations):
-
             title = f'{i+1}.Location: {str(location_instance).split(" ")[0]}'
+            location_type = str(location_instance).split(" ")[0]
             df = get_df_agents(agents=location_instance.agents)
-            df = df[[c for c  in agent_attributes]]
+
+            # only keep wanted columns (agent attributes)
+            df = df[list(agent_attributes)]
+
+            # add location type as str
+            df["location_type"] = location_type
+
             agent_dfs[title] = df
-    
-    
-    
-    for title, df in agent_dfs.items():
-        
+
+
         for agent_attribute in agent_attributes:
 
-            crosstab_table = pd.crosstab(index= df[agent_attribute],
-                                   columns="count",)
-            
-            print(f"{title}")
-            print(tabulate(crosstab_table,
-                           headers=[agent_attribute, "count"],
-                           tablefmt="fancy_grid"))
-            print(f"\n")
+            location_id = 0
+            df_list_of_attribute = []
+            for title, df in agent_dfs.items():
+
+                crosstab_table = pd.crosstab(
+                    index= df[agent_attribute],
+                    columns="count",
+                )
+                crosstab_table.insert(0, "location_id", [location_id]*len(crosstab_table.index))
+                crosstab_table["location_type"] = df["location_type"][0]
+
+                df_list_of_attribute.append(crosstab_table)
+
+                location_id += 1
+            df_attribute = pd.concat(df_list_of_attribute, ignore_index=False)
+
+            # fix columns after concat
+            df_attribute.reset_index(inplace=True)
+            df_attribute.columns.names = ["index"]
+            location_array = df_attribute["location_id"].copy()
+            df_attribute.drop(columns="location_id", inplace=True)
+            df_attribute.insert(0, "location_id", location_array)
+
+            result_list.append(df_attribute)
+        return result_list
+    return None
+
 
 ##########################################################################
 # technical helper functions
