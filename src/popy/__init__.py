b3d52e5a045aea9e594af21f35ebd2c9a832ff02
m <marius.kaffai@sowi.uni-stuttgart.de>
2024-07-25 13:01:30 +0200

    Add MeltLocations to __init__.py

--- src/popy/__init__.py
+++ src/popy/__init__.py
@@ -4,16 +4,9 @@ from agentpy import AgentList
 
 from .agent import Agent
 from .exceptions import PopyException
-#from .environment import Environment
-#from .location import GridLocation
-#from .location import LineLocation
-from .location import Location, MagicLocation
+from .location import Location, MagicLocation, MeltLocation
 from .inspector import NetworkInspector
-#from .location import RingLocation
-#from .location import StarLocation
-#from .location import TreeLocation
 from .model import Model
-#from .model import FakeModel
 from .sequences import LocationList
 
 __all__ = [
@@ -22,14 +15,8 @@ __all__ = [
     "PopyException",
     "Location",
     "MagicLocation",
+    "MeltLocation",
     "NetworkInspector",
-    #"Environment",
-    #"LineLocation",
-    #"RingLocation",
-    #"GridLocation",
-    #"TreeLocation",
-    #"StarLocation",
     "Model",
-    #"FakeModel",
     "LocationList",
 ]
