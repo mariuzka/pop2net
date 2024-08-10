a8f80d338aab970be6b17d2d3e43324e2d939c0f
MR <87819708+schorki9@users.noreply.github.com>
2024-08-10 11:46:26 +0200

    added test version without creator

--- tests/test_agent_shared_locations.py
+++ tests/test_agent_shared_locations.py
@@ -1,10 +1,46 @@
-# %%
 import popy
 
 
-def test_1():
+def test_1a():
+
+    class Max(popy.Agent):
+        pass
+
+    class Marius(popy.Agent):
+        pass
+
+    class Lukas(popy.Agent):
+        pass
+
+    class Meeting1(popy.MagicLocation):
+        pass
+
+    class Meeting2(popy.MagicLocation):
+        pass
+
     model = popy.Model()
-    creator = popy.Creator(model)
+    agent_max = Max(model = model)
+    agent_marius = Marius(model = model)
+    agent_lukas = Lukas(model = model)
+    meeting1 = Meeting1(model = model)
+    meeting2 = Meeting2(model = model)
+    meeting1.add_agents(agents = [agent_max, agent_marius])
+    meeting2.add_agents(agents = [agent_marius, agent_lukas])
+
+    assert len(model.locations) == 2
+    assert len(model.agents) == 3
+    assert agent_max.shared_locations(agent=agent_marius)[0].type == "Meeting1"
+    assert not bool(agent_max.shared_locations(agent=agent_lukas))
+    assert agent_marius.shared_locations(agent=agent_max)[0].type == "Meeting1"
+    assert agent_marius.shared_locations(agent=agent_lukas)[0].type == "Meeting2"
+    assert agent_lukas.shared_locations(agent=agent_marius)[0].type == "Meeting2"
+    assert not bool(agent_lukas.shared_locations(agent=agent_max))
+
+
+
+def test_1b():
+    model = popy.Model()
+    creator = popy.Creator(model = model)
 
     class Max(popy.Agent):
         pass
@@ -31,7 +67,6 @@ def test_1():
     assert len(model.locations) == 2
     assert len(model.agents) == 3
     assert _max.shared_locations(agent=_marius)[0].type == "Meeting1"
-    # TODO offiziellster Weg leere Liste zu checken?
     assert not bool(_max.shared_locations(agent=_lukas))
     assert _marius.shared_locations(agent=_max)[0].type == "Meeting1"
     assert _marius.shared_locations(agent=_lukas)[0].type == "Meeting2"
