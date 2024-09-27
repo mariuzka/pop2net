06cd67a1443cc08dd77d5f18bab95fad75362f5f
m <marius.kaffai@sowi.uni-stuttgart.de>
2024-09-27 17:58:20 +0200

    Add option to clear population

---
+++ tests/test_creator/test_creator_create_clear.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+import pop2net as p2n
+
+
+def test_create_agents():
+    model = p2n.Model()
+    creator = p2n.Creator(model=model)
+
+    creator.create_agents(n=10, clear=False)
+
+    assert len(model.agents) == 10
+
+    creator.create_agents(n=10, clear=False)
+
+    assert len(model.agents) == 20
+
+    creator.create_agents(n=10, clear=True)
+
+    assert len(model.agents) == 10
+
+
+def test_create_locations():
+    model = p2n.Model()
+    creator = p2n.Creator(model=model)
+
+    class MyLocation(p2n.MagicLocation):
+        n_locations = 5
+
+    creator.create_agents(n=10)
+    creator.create_locations(location_classes=[MyLocation], clear=False)
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 5
+
+    creator.create_locations(location_classes=[MyLocation], clear=False)
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 10
+
+    creator.create_locations(location_classes=[MyLocation], clear=True)
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 5
+
+
+def test_create():
+    df = pd.DataFrame({"age": [10, 20, 30, 40, 50]})
+
+    model = p2n.Model()
+    creator = p2n.Creator(model=model)
+
+    class MyLocation(p2n.MagicLocation):
+        n_locations = 5
+
+    creator.create(
+        n_agents=10,
+        df=df,
+        location_classes=[MyLocation],
+        clear_agents=False,
+        clear_locations=False,
+    )
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 5
+
+    creator.create(
+        n_agents=10,
+        df=df,
+        location_classes=[MyLocation],
+        clear_agents=False,
+        clear_locations=False,
+    )
+
+    assert len(model.agents) == 20
+    assert len(model.locations) == 10
+
+    creator.create(
+        n_agents=10,
+        df=df,
+        location_classes=[MyLocation],
+        clear_agents=True,
+        clear_locations=False,
+    )
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 15
+
+    creator.create(
+        n_agents=10,
+        df=df,
+        location_classes=[MyLocation],
+        clear_agents=False,
+        clear_locations=True,
+    )
+
+    assert len(model.agents) == 20
+    assert len(model.locations) == 5
+
+    creator.create(
+        n_agents=10,
+        df=df,
+        location_classes=[MyLocation],
+        clear_agents=True,
+        clear_locations=True,
+    )
+
+    assert len(model.agents) == 10
+    assert len(model.locations) == 5
