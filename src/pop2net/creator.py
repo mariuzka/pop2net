a0d0675ae061266965bd13a997a4daff91574e8f
mariuzka <mariuzka@users.noreply.github.com>
2025-07-02 13:27:35 +0000

    auto-commit: fixes by ruff

--- src/pop2net/creator.py
+++ src/pop2net/creator.py
@@ -944,7 +944,7 @@ class Creator:
         replace_sample_level_column: bool = True,
         clear: bool = False,
         delete_magic_actor_attributes: bool = True,
-        return_elements = False,
+        return_elements=False,
     ) -> tuple:
         """Creates actors and locations based on a given dataset.
 
