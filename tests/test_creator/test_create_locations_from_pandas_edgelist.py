4abf3ea6d398cfd73a2dfb256f2b170b8a382872
mariuzka <mariuzka@users.noreply.github.com>
2026-07-08 15:58:17 +0000

    auto-commit: fixes by ruff

--- tests/test_creator/test_create_locations_from_pandas_edgelist.py
+++ tests/test_creator/test_create_locations_from_pandas_edgelist.py
@@ -1,14 +1,16 @@
 import pandas as pd
+
 import pop2net as p2n
 
+
 def test_simple():
-    df_actors = pd.DataFrame({
-        "name": ["John", "Paul", "Gustav"]
-    })
-    df_edges = pd.DataFrame([
-        {"source": "John", "target": "Paul", "weight": 1},
-        {"source": "John", "target": "Gustav", "weight": 3},
-    ])
+    df_actors = pd.DataFrame({"name": ["John", "Paul", "Gustav"]})
+    df_edges = pd.DataFrame(
+        [
+            {"source": "John", "target": "Paul", "weight": 1},
+            {"source": "John", "target": "Gustav", "weight": 3},
+        ]
+    )
 
     env = p2n.Environment()
     creator = p2n.Creator(env=env)
@@ -26,30 +28,32 @@ def test_simple():
     assert env.locations[0].label == "TestLocation"
 
     assert (
-        env._get_actor_by_attr_value("name", "John") 
+        env._get_actor_by_attr_value("name", "John")
         in env._get_actor_by_attr_value("name", "Paul").neighbors()
     )
 
     assert (
-        env._get_actor_by_attr_value("name", "Gustav") 
+        env._get_actor_by_attr_value("name", "Gustav")
         in env._get_actor_by_attr_value("name", "John").neighbors()
     )
 
     assert (
-        env._get_actor_by_attr_value("name", "Gustav") 
+        env._get_actor_by_attr_value("name", "Gustav")
         not in env._get_actor_by_attr_value("name", "Paul").neighbors()
     )
 
+
 def test_with_framework():
-    df_actors = pd.DataFrame({
-        "name": ["John", "Paul", "Gustav"]
-    })
-    df_edges = pd.DataFrame([
-        {"source": "John", "target": "Paul", "weight": 1},
-        {"source": "John", "target": "Gustav", "weight": 3},
-    ])
+    df_actors = pd.DataFrame({"name": ["John", "Paul", "Gustav"]})
+    df_edges = pd.DataFrame(
+        [
+            {"source": "John", "target": "Paul", "weight": 1},
+            {"source": "John", "target": "Gustav", "weight": 3},
+        ]
+    )
 
     import mesa
+
     model = mesa.Model()
     env = p2n.Environment(model=model, framework="mesa")
     creator = p2n.Creator(env=env)
@@ -67,16 +71,16 @@ def test_with_framework():
     assert env.locations[0].label == "TestLocation"
 
     assert (
-        env._get_actor_by_attr_value("name", "John") 
+        env._get_actor_by_attr_value("name", "John")
         in env._get_actor_by_attr_value("name", "Paul").neighbors()
     )
 
     assert (
-        env._get_actor_by_attr_value("name", "Gustav") 
+        env._get_actor_by_attr_value("name", "Gustav")
         in env._get_actor_by_attr_value("name", "John").neighbors()
     )
 
     assert (
-        env._get_actor_by_attr_value("name", "Gustav") 
+        env._get_actor_by_attr_value("name", "Gustav")
         not in env._get_actor_by_attr_value("name", "Paul").neighbors()
     )
